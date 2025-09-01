481f82ef9fac1c4640e6b792a251ade84dca70f1
bion23ouriquess <gaby8.bion@gmail.com>
2025-09-01 19:31:41 -0300

    first commit

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-export const prismaClient:PrismaClient = new PrismaClient({
+export const prismaClient = new PrismaClient({
     log: ['query', 'info', 'warn', 'error'],
 });
